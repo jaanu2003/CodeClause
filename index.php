3aee1bcbac24c015dd205f903332bd2e7723f482
jahnaviurugonda <93399446+jaanu2003@users.noreply.github.com>
2022-10-31 22:19:04 +0530

    files

--- index.php
+++ index.php
@@ -1,156 +1,166 @@
-<?php 
+<?php
+include('connection.php');
 session_start();
-include('../connection.php');
-$user= $_SESSION['user'];
-$sql=mysqli_query($conn,"select * from user where email='$user' ");
-$users=mysqli_fetch_assoc($sql);
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+ ?>
+<html>
+	<head>
+		<title>Online Notice Board</title>
+		<link rel="stylesheet" href="css/bootstrap.css"/>
+		<script src="js/jquery_library.js"></script>
+<script src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+			<nav class="navbar navbar-default navbar-fixed-top" style="background:#000">
+  <div class="container">
 
-    <title>Online Notice Board User Dashboard</title>
+  <ul class="nav navbar-nav navbar-left">
+    <li><a href="index.php"><strong>Online Notice Board</strong></a></li>
 
-    <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+	  <li><a href="index.php?option=about"><span class="glyphicon glyphicon-user"></span> About</a></li>
 
-    <!-- Custom styles for this template -->
-    <link href="../css/dashboard.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../js/ie-emulation-modes-warning.js"></script>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+	<li><a href="index.php?option=contact"><span class="glyphicon glyphicon-phone"></span>Contact</a></li>
 
-  <body>
+	</ul>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Hello <?php echo $users['name'];?></a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-           
-            <li><a href="logout.php">Logout</a></li>
-          </ul>
-          <!--<form class="navbar-form navbar-right">
-            <input type="text" class="form-control" placeholder="Search...">
-          </form>-->
-        </div>
+
+<ul class="nav navbar-nav navbar-right">
+      <li><a href="index.php?option=New_user"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="index.php?option=login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+
+
+
+</div>
+</nav>
+
+<div class="container-fluid">
+	<!-- slider -->
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/sitePHP.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
       </div>
-    </nav>
-
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="index.php">Dashboard <span class="sr-only">(current)</span></a></li>
-			<!-- find users' image if image not found then show dummy image -->
-			
-			<!-- check users profile image -->
-			<?php 
-			$q=mysqli_query($conn,"select image from user where email='".$_SESSION['user']."'");
-			$row=mysqli_fetch_assoc($q);
-			if($row['image']=="")
-			{
-			?>
-            <li><a href="index.php?page=update_profile_pic"><img title="Update Your profile pic Click here" style="border-radius:50px" src="../images/person.jpg" width="100" height="100" alt="not found"/></a></li>
-			<?php 
-			}
-			else
-			{
-			?>
-			<li><a href="index.php?page=update_profile_pic"><img title="Update Your profile pic Click here"  style="border-radius:50px" src="../images/<?php echo $_SESSION['user'];?>/<?php echo $row['image'];?>" width="100" height="100" alt="not found"/></a></li>
-			<?php 
-			}
-			?>
-			
-			
-			
-			<li><a href="index.php?page=update_password"><span class="glyphicon glyphicon-user"></span> Update Password</a></li>
-            <li><a href="index.php?page=update_profile"><span class="glyphicon glyphicon-user"></span> Update Profile</a></li>
-			 <li><a href="index.php?page=notification"><span class="glyphicon glyphicon-envelope"></span> Notification</a></li>
-            
-          </ul>
-         
-         
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <!-- container-->
-		  <?php 
-		@$page=  $_GET['page'];
-		  if($page!="")
-		  {
-		  	if($page=="update_password")
-			{
-				include('update_password.php');
-			
-			}
-			if($page=="notification")
-			{
-				include('notification.php');
-			
-			}
-			if($page=="update_profile")
-			{
-				include('update_profile.php');
-			
-			}
-			if($page=="update_profile_pic")
-			{
-				include('update_profile_pic.php');
-			
-			}
-		  }
-		  else
-		  {
-		  include('notification.php');
-		  ?>
-		  <!-- container end-->
-		   
-		  
-		  <h1 class="page-header">Dashboard</h1>
-		  
-		  
-<?php } ?>
-          
-         
-        </div>
+    </div>
+    <div class="item">
+      <img src="images/notice.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
       </div>
     </div>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="../js/vendor/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+	 <div class="item">
+      <img src="images/notice2.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- slider end-->
+</div>
+
+
+<div class="container">
+	<div class="row">
+	<!-- container -->
+		<div class="col-sm-8">
+		<?php
+		@$opt=$_GET['option'];
+
+		if($opt!="")
+		{
+			if($opt=="about")
+			{
+			include('about.php');
+			}
+			else if($opt=="contact")
+			{
+			include('contact.php');
+			}
+
+			else if($opt=="New_user")
+			{
+			include('registration.php');
+			}
+
+
+			else if($opt=="login")
+			{
+			include('login.php');
+			}
+		}
+		else
+		{
+		echo "<h2><b>'WELCOME USER TO OUR ONLINE NOTICE BOARD'</b></h2>
+		<i> <b> Join us today and get connected. Register now to get each and every updates of your college! </b></i>";
+		}
+		?>
+
+
+
+		</div>
+	<!-- container -->
+
+		<div class="col-sm-4">
+			<div class="panel panel-default">
+  <div class="panel-heading"><b>LATEST NEWS</b></div>
+  <div class="panel-body">
+    ... College now uses Google Apps for Education for fast and easy collaboration. The products that we use includes Gmail, Calendar, Docs and more.With Google Apps for Education, everything is automatically saved in the cloud – 100% powered by the web. This means that emails, documents, calendar and sites can be accessed – and edited – on almost any mobile device or tablet. Anytime, anywhere.
+  </div>
+</div>
+
+		</div>
+	</div>
+
+</div>
+
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+<!-- footer-->
+
+			<nav class="navbar navbar-default navbar-bottom" style="background:black">
+  <div class="container">
+
+  <ul class="nav navbar-nav navbar-left">
+    <li><a href="https://code-projects.org/"> Brought To You By Code-Projects</a></li>
+
+	</ul>
+
+
+
+
+</div>
+</nav>
+<!-- footer-->
+
+	</body>
 </html>
--- login.php
+++ login.php
@@ -1,108 +1,62 @@
 <?php
-	include('../connection.php');
-	session_start();
-	extract($_POST);
-	if(isset($login))
+extract($_POST);
+if(isset($save))
+{
+
+	if($e=="" || $p=="")
 	{
-		$que=mysqli_query($conn,"select * from admin where user='$email' and pass='$pass'");
-		$row=mysqli_num_rows($que);
-		if($row)
-			{	
-				$_SESSION['admin']=$email;
-				header('location:index.php');
-			}
-		else
-			{
-				$err="<font color='red'>Wrong Email or Password !</font>";
-			}
+	$err="<font color='red'>fill all the fileds first</font>";
 	}
+	else
+	{
+$pass=md5($p);
+
+$sql=mysqli_query($conn,"select * from user where email='$e' and pass='$pass'");
+
+$r=mysqli_num_rows($sql);
+
+if($r==true)
+{
+$_SESSION['user']=$e;
+header('location:user');
+}
+
+else
+{
+
+$err="<font color='red'>Invalid login details</font>";
+
+}
+}
+}
+
 ?>
+<h2><b>LOGIN FORM</B></h2>
+<form method="post">
 
-<!DOCTYPE html>
-<html lang="en">
+	<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
 
-<head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Admin !</title>
+	<div class="row">
+		<div class="col-sm-4">Email ID</div>
+		<div class="col-sm-5">
+		<input type="email" name="e" class="form-control"/></div>
+	</div>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
+	<div class="row">
+		<div class="col-sm-4">Password</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+		<input type="submit" value="Login" name="save" class="btn btn-success"/>
 
-    <!-- MetisMenu CSS -->
-    <link href="../css/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign In</h3>
-                    </div>
-                    <div class="panel-body">
-                        <form method="post">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" name="email" type="email" autofocus required placeholder="E-mail">
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="pass" type="password" required>
-                                </div>
-                                
-                                
-								<div class="form-group">
-                                    <input name="login" type="submit" value="Login" class="btn btn-lg btn-success btn-block">
-                                </div>
-								
-								<div class="form-group">
-                                    <label>
-                                        <?php echo @$err;?>
-                                    </label>
-                                </div>
-								
-                                
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- jQuery -->
-    <script src="../css/css/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../css/css/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../css/css/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../css/css/sb-admin-2.js"></script>
-
-</body>
-
-</html>
+		</div>
+	</div>
+</form>
